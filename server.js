86ca450848bc8d994557c120101785cb0a0c72d2
raspi_flo <test@example.com>
2024-03-28 16:11:40 +0100

    alles rauf bro

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+const { createServer } = require('node:http');
+
+const hostname = '10.62.2.27';
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+const server = createServer((req, res) => {
+  console.log(`Connection got!`);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'html');
+  res.end('Hello World');
+});
+*/
+
+app.get('/', async(req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
